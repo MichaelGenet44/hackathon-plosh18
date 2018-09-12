ab18b59146e5d5129b21a728b27bb8ddf07f14ac
mika-44 <michael.genet@insee.fr>
2018-09-12 10:05:52 +0200

    improvements

--- tourisme/src/main/java/eu/europa/ec/eurostat/los/codes/DataCubeMaker.java
+++ tourisme/src/main/java/eu/europa/ec/eurostat/los/codes/DataCubeMaker.java
@@ -5,6 +5,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Map;
 
+import org.apache.jena.datatypes.xsd.XSDDatatype;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.Property;
@@ -38,21 +39,24 @@ public class DataCubeMaker {
 			tourismDepModel.setNsPrefix("xsd", XSD.getURI());
 			tourismDepModel.setNsPrefix("qb", DataCubeOntology.getURI());
 
-			tourismDepModel.setNsPrefix("dim", "http://id.insee.fr/meta/dimension/");
-			tourismDepModel.setNsPrefix("mes", "http://id.insee.fr/meta/mesure/");
+			tourismDepModel.setNsPrefix("dim", "http://id.linked-open-statistics.org/meta/dimension/");
+			tourismDepModel.setNsPrefix("mes", "http://id.linked-open-statistics.org/meta/mesure/");
+			tourismDepModel.setNsPrefix("att", "http://id.linked-open-statistics.org/meta/attribute/");
+			tourismDepModel.setNsPrefix("sdmxdim", "http://purl.org/linked-data/sdmx/2009/dimension#");
+			
 			Resource tourismDataSet = tourismDepModel.createResource(Configuration.BASE_URI + "dataSet/tourism-nuts-nacer2-occ-arr", DataCubeOntology.DataSet);
 	
 			// Dimensions and measure
 			Property cresidDimensionProperty = tourismDepModel.createProperty(Configuration.componentURI("dimension", "C_RESID"));
 			Property nutsDimensionProperty = tourismDepModel.createProperty(Configuration.componentURI("dimension", "NUTS"));
 			Property nacer2DimensionProperty = tourismDepModel.createProperty(Configuration.componentURI("dimension", "NACE_R2"));
-			Property timePeriodDimensionProperty = tourismDepModel.createProperty(Configuration.componentURI("dimension", "TIME_PERIOD"));
-			Property measureProperty = tourismDepModel.createProperty(Configuration.componentURI("dimension", "MEASURE"));
+			Property timePeriodDimensionProperty = tourismDepModel.createProperty("http://purl.org/linked-data/sdmx/2009/dimension#timePeriod");
+			Property measureProperty = tourismDepModel.createProperty(Configuration.componentURI("mesure", "MEASURE"));
 			//Property observationProperty = tourismDepModel.createProperty(Configuration.componentURI("dimension", "OBS_VALUE"));
 
-			Property obsStatusAttributProperty = tourismDepModel.createProperty(Configuration.componentURI("dimension", "OBS_STATUS"));
-			Property confStatusAttributProperty = tourismDepModel.createProperty(Configuration.componentURI("dimension", "CONF_STATUS"));
-			Property unitAttributProperty = tourismDepModel.createProperty(Configuration.componentURI("dimension", "UNIT"));
+			Property obsStatusAttributProperty = tourismDepModel.createProperty(Configuration.componentURI("attribute", "OBS_STATUS"));
+			Property confStatusAttributProperty = tourismDepModel.createProperty(Configuration.componentURI("attribute", "CONF_STATUS"));
+			Property unitAttributProperty = tourismDepModel.createProperty(Configuration.componentURI("attribute", "UNIT"));
 			
 			CSVReaderHeaderAware reader = new CSVReaderHeaderAware(fileReader);
 			Map<String, String> nextLine;
@@ -89,8 +93,7 @@ public class DataCubeMaker {
 		observation.addProperty(nutsDimensionProperty, nutsResource);
 		Resource nacer2Resource = tourismDepModel.createResource(Configuration.codeItemURI("NACE_R2", nextLine.get("NACE_R2")));
 		observation.addProperty(nacer2DimensionProperty, nacer2Resource);
-		Resource timeResource = tourismDepModel.createResource(Configuration.codeItemURI("TIME_PERIOD", nextLine.get("TIME_PERIOD")));
-		observation.addProperty(timePeriodDimensionProperty, timeResource);
+		observation.addProperty(timePeriodDimensionProperty, tourismDepModel.createTypedLiteral(nextLine.get("TIME_PERIOD"), XSDDatatype.XSDgYear));
 		Resource obsStatusResource = tourismDepModel.createResource(Configuration.codeItemURI("OBS_STATUS", nextLine.get("OBS_STATUS")));
 		observation.addProperty(obsStatusAttributProperty, obsStatusResource);
 		Resource confStatusResource = tourismDepModel.createResource(Configuration.codeItemURI("CONF_STATUS", nextLine.get("CONF_STATUS")));
@@ -98,9 +101,8 @@ public class DataCubeMaker {
 		Resource unitResource = tourismDepModel.createResource(Configuration.codeItemURI("UNIT", nextLine.get("UNIT")));
 		observation.addProperty(unitAttributProperty, unitResource);
 		// Add measure
-		observation.addProperty(measureProperty, tourismDepModel.createTypedLiteral(nextLine.get("OBS_VALUE")));
+		observation.addProperty(measureProperty, tourismDepModel.createTypedLiteral(Integer.valueOf(nextLine.get("OBS_VALUE"))));
 	}
-
 
 	
 }
